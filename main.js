0e540989e011b772ca1af3399a34d4598634d472
MegaFire1121 <Vincentz1116@aol.com>
2026-03-07 23:44:22 -0500

    10m

--- main.js
+++ main.js
@@ -24,3 +24,15 @@ document.addEventListener("DOMContentLoaded", () => {
     form.reset();
   });
 });
+
+
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("nav-links");
+
+hamburger.addEventListener("click", () => {
+  if (navLinks.style.display === "flex") {
+    navLinks.style.display = "none";
+  } else {
+    navLinks.style.display = "flex";
+  }
+});
